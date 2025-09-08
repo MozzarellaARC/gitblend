a1bea93ad1778fccb51c4c2e3ffdd26122784185
Mozzie <donytiara@gmail.com>
2025-09-09 00:14:34 +0700

    clean

--- main/__init__.py
+++ main/__init__.py
@@ -2,43 +2,15 @@ import bpy # type: ignore # type: ignore
 
 from .operators import (
     GITBLEND_OT_commit,
-    GITBLEND_OT_initialize,
-    GITBLEND_OT_string_add,
-    GITBLEND_OT_string_remove,
-    GITBLEND_OT_undo_commit,
-    GITBLEND_OT_discard_changes,
-    GITBLEND_OT_checkout_log,
-    GITBLEND_OT_append_objects,
 )
 
 __all__ = (
-    "register_main",
-    "unregister_main",
+    "register_operators",
+    "unregister_operators",
 )
 
-def register_main():
+def register_operators():
     bpy.utils.register_class(GITBLEND_OT_commit)
-    bpy.utils.register_class(GITBLEND_OT_initialize)
-    bpy.utils.register_class(GITBLEND_OT_string_add)
-    bpy.utils.register_class(GITBLEND_OT_string_remove)
-    bpy.utils.register_class(GITBLEND_OT_undo_commit)
-    bpy.utils.register_class(GITBLEND_OT_discard_changes)
-    bpy.utils.register_class(GITBLEND_OT_checkout_log)
-    bpy.utils.register_class(GITBLEND_OT_append_objects)
-
-
-def unregister_main():
-    bpy.utils.unregister_class(GITBLEND_OT_discard_changes)
-    bpy.utils.unregister_class(GITBLEND_OT_undo_commit)
-    bpy.utils.unregister_class(GITBLEND_OT_string_remove)
-    bpy.utils.unregister_class(GITBLEND_OT_string_add)
-    bpy.utils.unregister_class(GITBLEND_OT_initialize)
+    
+def unregister_operators():
     bpy.utils.unregister_class(GITBLEND_OT_commit)
-    try:
-        bpy.utils.unregister_class(GITBLEND_OT_checkout_log)
-    except RuntimeError:
-        pass
-    try:
-        bpy.utils.unregister_class(GITBLEND_OT_append_objects)
-    except RuntimeError:
-        pass
--- prefs/__init__.py
+++ prefs/__init__.py
@@ -2,12 +2,9 @@ import bpy # type: ignore # type: ignore
 
 from .properties import (
 	GITBLEND_Properties,
-	GITBLEND_StringItem,
-	GITBLEND_ChangeLogEntry,
 )
 
 from .panel import (
-	GITBLEND_UL_ChangeLog,
 	GITBLEND_Panel,
 )
 
@@ -16,47 +13,10 @@ __all__ = (
 	"unregister_properties",
 )
 
-
-def _safe_register(cls):
-	try:
-		bpy.utils.register_class(cls)
-	except ValueError:
-		# Already registered; ignore to prevent noisy errors during dev reloads
-		pass
-
-
-def _safe_unregister(cls):
-	try:
-		bpy.utils.unregister_class(cls)
-	except RuntimeError:
-		pass
-	except ValueError:
-		pass
-
-
 def register_properties():
-	# Register PropertyGroups first
-	_safe_register(GITBLEND_ChangeLogEntry)
-	_safe_register(GITBLEND_StringItem)
-	_safe_register(GITBLEND_Properties)
-	_safe_register(GITBLEND_UL_ChangeLog)
-	_safe_register(GITBLEND_Panel)
-	
-	# Attach to Scene once
-	if not hasattr(bpy.types.Scene, "gitblend_props"):
-		bpy.types.Scene.gitblend_props = bpy.props.PointerProperty(type=GITBLEND_Properties)
+	bpy.utils.register_class(GITBLEND_Properties)
+	bpy.utils.register_class(GITBLEND_Panel)
 
 def unregister_properties():
-	# Remove Scene property first to drop references
-	if hasattr(bpy.types.Scene, "gitblend_props"):
-		try:
-			del bpy.types.Scene.gitblend_props
-		except Exception:
-			pass
-	# Unregister PropertyGroups (reverse order)
- 
-	_safe_unregister(GITBLEND_Properties)
-	_safe_unregister(GITBLEND_StringItem)
-	_safe_unregister(GITBLEND_ChangeLogEntry)
-	_safe_unregister(GITBLEND_UL_ChangeLog)
-	_safe_unregister(GITBLEND_Panel)
+	bpy.utils.unregister_class(GITBLEND_Panel)
+	bpy.utils.unregister_class(GITBLEND_Properties)
