ff4af79e7c43d64ad95ec1022bf236618769f941
Mozzie <donytiara@gmail.com>
2025-09-19 08:07:22 +0700

    prep

--- prefs/properties.py
+++ prefs/properties.py
@@ -1,1 +1,8 @@
 import bpy  # type: ignore
+
+class GITBLEND_Properties(bpy.types.PropertyGroup):
+    commit_message: bpy.props.StringProperty(
+        name="Commit Message",
+        description="Message for the commit",
+        default=""
+    )
