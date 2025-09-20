db2a1a9def0dac40645cc4188855ff7611258f1e
Mozzie <donytiara@gmail.com>
2025-09-20 13:16:26 +0700

    restructure

--- prefs/panel.py
+++ prefs/panel.py
@@ -13,7 +13,7 @@ class GITBLEND_UL_commit_history(bpy.types.UIList):
             
             # Hash (shortened)
             col = split.column()
-            col.label(text=f"#{item.hash[:8]}")
+            col.label(text=f"{item.hash[:8]}")
             
             # Timestamp
             col = split.column()
