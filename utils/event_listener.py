ac8535c3827b90d5e773fc733fcdfc105f163bef
Mozzie <donytiara@gmail.com>
2025-09-22 00:38:15 +0700

    hello world

--- utils/event_listener.py
+++ utils/event_listener.py
@@ -1,8 +1,7 @@
 import bpy
 
 def on_save_event_listener(self, context):
-	#TODO: Staging area
-    pass
+	print("Hello World!")
 
 def invoke_save_event_listener():
 	bpy.app.handlers.save_post.append(on_save_event_listener)
