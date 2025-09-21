7098a7d6907f27b5c6b332e1d7810b11d506e508
Mozzie <donytiara@gmail.com>
2025-09-21 22:25:43 +0700

    working import

--- main/checkout.py
+++ main/checkout.py
@@ -2,26 +2,26 @@ import bpy
 from pathlib import Path
 
 class GITBLEND_OT_Checkout(bpy.types.Operator):
-    """Checkout a specific commit"""
-    bl_idname = "gitblend.checkout"
-    bl_label = "Checkout Commit"
-    bl_options = {'REGISTER', 'UNDO'}
+	"""Checkout a specific commit"""
+	bl_idname = "gitblend.checkout"
+	bl_label = "Checkout Commit"
+	bl_options = {'REGISTER', 'UNDO'}
 
 
-    def execute(self, context):
+	def execute(self, context):
 
-        scene = context.scene.gitblend_props
-        gitblend_dir = Path(bpy.data.filepath).parent / ".gitblend"
-        index = scene.commit_history[scene.i]
-        filepath = gitblend_dir / index.filename
+		scene = context.scene.gitblend_props
+		gitblend_dir = Path(bpy.data.filepath).parent / ".gitblend"
+		index = scene.commit_history[scene.i]
+		filepath = gitblend_dir / index.filename
 
-        with bpy.data.libraries.load(str(filepath), link=False) as (data_from, data_to):
-            # Loead based on message
-            data_to.objects = [name for name in data_from.objects if name.startswith(index.message)]
-            
-        # Link the loaded objects to the current scene
-        for obj in data_to.objects:
-            if obj is not None:
-                context.collection.objects.link(obj)
-
-        return {'FINISHED'}
+		with bpy.data.libraries.load(str(filepath), link=False) as (data_from, data_to):
+			# Loead based on message
+			data_to.objects = [name for name in data_from.objects if name.startswith(index.message)]
+			
+		# Link the loaded objects to the current scene
+		for obj in data_to.objects:
+			if obj is not None:
+				context.collection.objects.link(obj)
+				
+		return {'FINISHED'}
