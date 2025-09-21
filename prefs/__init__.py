0f049395cfb2349b07719e81442047fbee30951c
Mozzie <donytiara@gmail.com>
2025-09-21 17:26:49 +0700

    panel simple

--- prefs/__init__.py
+++ prefs/__init__.py
@@ -1,25 +1,23 @@
 import bpy # type: ignore # type: ignore
 
 from .properties import (
-	register_properties,
-	unregister_properties,
+	GITBLEND_Properties,
 )
 
 from .panel import (
 	GITBLEND_PT_Panel,
-	GITBLEND_UL_commit_history,
 )
 
 
 def register_prefs():
-	register_properties()
-	bpy.utils.register_class(GITBLEND_UL_commit_history)
+	bpy.utils.register_class(GITBLEND_Properties)
+	bpy.types.Scene.gitblend_props = bpy.props.PointerProperty(type=GITBLEND_Properties)
 	bpy.utils.register_class(GITBLEND_PT_Panel)
 
 def unregister_prefs():
 	bpy.utils.unregister_class(GITBLEND_PT_Panel)
-	bpy.utils.unregister_class(GITBLEND_UL_commit_history)
-	unregister_properties()
+	del bpy.types.Scene.gitblend_props
+	bpy.utils.unregister_class(GITBLEND_Properties)
 
 __all__ = (
 	"register_prefs",
--- prefs/panel.py
+++ prefs/panel.py
@@ -1,9 +1,6 @@
-import bpy # type: ignore
-from .constants import GITBLEND_DIR_NAME
-from pathlib import Path
+import bpy
 
 class GITBLEND_PT_Panel(bpy.types.Panel):
-    """Panel for Git Blend preferences."""
     bl_label = "Git Blend"
     bl_idname = "GITBLEND_PT_panel"
     bl_space_type = 'VIEW_3D'
@@ -11,4 +8,10 @@ class GITBLEND_PT_Panel(bpy.types.Panel):
     bl_category = 'Git Blend'
 
     def draw(self, context):
-        pass
+        layout = self.layout
+        scene = context.scene.gitblend_props
+
+        row0 = layout.row()
+        row0.prop(scene, "commit_message", icon='TEXT', text='')
+        row1 = layout.row()
+        row1.operator("gitblend.commit", text="Commit Changes", icon='FILE_TICK')
