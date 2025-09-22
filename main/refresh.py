c7469884af15d4048487a6294be832d982552d86
Mozzie <donytiara@gmail.com>
2025-09-22 10:39:48 +0700

    more data

--- main/refresh.py
+++ main/refresh.py
@@ -76,6 +76,39 @@ class GITBLEND_OT_Serde(bpy.types.Operator):
             obj_ptrs = obj.data
             print(f"{obj.name} -> {obj_ptrs.name}")
             # Get all modifiers
+            print("Modifiers:")
             for mod in obj.modifiers:
                 print(f"-> {mod.name}")
+            
+            print("Particles:")
+            for p in obj.particle_systems:
+                print(f"-> {p.name}")
+
+            # Physics are not itterable, so we check if they exist
+
+            print("Constraints:")
+            for con in obj.constraints:
+                print(f"-> {con.name}")
+
+            print("Data:")
+            if obj_ptrs is not None:
+                print(f"-> {obj_ptrs.name} ({obj_ptrs.__class__.__name__})")
+                if obj_ptrs.__class__.__name__ == "Mesh":
+                    mesh = obj_ptrs
+                    print("  Vertices:")
+                    for v in mesh.vertices:
+                        print(f"  -> {v.index}: {v.co}")
+                    print("  Edges:")
+                    for e in mesh.edges:
+                        print(f"  -> {e.index}: {e.vertices[:]}")
+                    print("  Faces:")
+                    for f in mesh.polygons:
+                        print(f"  -> {f.index}: {f.vertices[:]}")
+            else:
+                print("-> No data")
+
+            print("Materials:")
+            for mat in obj.data.materials:
+                print(f"-> {mat.name}")
+
         return {'FINISHED'}
